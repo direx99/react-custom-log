0e17625e6995d28846e35d9327c94aab38a2d8a0
direx99 <dinith.dash@gmail.com>
2024-07-13 20:21:25 +0530

    feat: :sparkles: Colored Log
    Colored logs feture added with 5 pre defined colored logs.

---
+++ index.js
@@ -0,0 +1,14 @@
+// Colored logs
+const logWithColor = (message, color) => {
+  console.log(`%c${message}`, `color: ${color}`);
+};
+
+const CustomLog = {
+  Default: (message) => logWithColor(message, ""),
+  Info: (message) => logWithColor(message, "blue"),
+  Error: (message) => logWithColor(message, "red"),
+  Warn: (message) => logWithColor(message, "orange"),
+  Processing: (message) => logWithColor(message, "purple"),
+};
+
+export default CustomLog;
