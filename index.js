0773b3356cbf7a23443bbb757286b3601a9d89e9
direx99 <dinith.dash@gmail.com>
2024-07-13 20:40:56 +0530

    feat: :zap: Added JSON support log.

--- index.js
+++ index.js
@@ -4,12 +4,23 @@ const logWithColor = (message, color) => {
 };
 
 const CustomLog = {
+  //Colored Logs
   Default: (message) => logWithColor(message, ""),
   Success: (message) => logWithColor(message, "green"),
   Info: (message) => logWithColor(message, "blue"),
   Error: (message) => logWithColor(message, "red"),
   Warn: (message) => logWithColor(message, "orange"),
   Processing: (message) => logWithColor(message, "purple"),
+
+  // JSON Logs
+  PrettyJSON: (json) => {
+    try {
+      const prettyString = JSON.stringify(json, null, 2);
+      console.log(prettyString);
+    } catch (error) {
+      CustomLog.Error("Invalid JSON provided");
+    }
+  },
 };
 
 export default CustomLog;
