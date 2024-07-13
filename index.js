c2f97df5a92190083b14cf768af81a551c60e261
direx99 <dinith.dash@gmail.com>
2024-07-13 20:47:48 +0530

    feat: :zap: Colord JSON Log
    Now JSON log can be set color or if its success code/response then it will be green else red.

--- index.js
+++ index.js
@@ -1,26 +1,44 @@
-// Colored logs
+//Default colored LOG
 const logWithColor = (message, color) => {
   console.log(`%c${message}`, `color: ${color}`);
 };
 
+//JSON colored LOG
+const logJsonWithColor = (json) => {
+  try {
+    const prettyString = JSON.stringify(json, null, 2);
+    let color = "black";
+
+    if (
+      (json.status && json.status === "error") ||
+      (json.statusCode && json.statusCode !== 200) ||
+      (json.status_code && json.status_code !== 200) ||
+      (json.code && json.code !== 200)
+    ) {
+      color = "red";
+    } else if (
+      (json.status && json.status === "success") ||
+      (json.statusCode && json.statusCode === 200) ||
+      (json.status_code && json.status_code === 200) ||
+      (json.code && json.code === 200)
+    ) {
+      color = "green";
+    }
+
+    logWithColor(prettyString, color);
+  } catch (error) {
+    CustomLog.Error("Invalid JSON provided");
+  }
+};
+
 const CustomLog = {
-  //Colored Logs
   Default: (message) => logWithColor(message, ""),
-  Success: (message) => logWithColor(message, "green"),
   Info: (message) => logWithColor(message, "blue"),
   Error: (message) => logWithColor(message, "red"),
+  Success: (message) => logWithColor(message, "green"),
   Warn: (message) => logWithColor(message, "orange"),
   Processing: (message) => logWithColor(message, "purple"),
-
-  // JSON Logs
-  PrettyJSON: (json) => {
-    try {
-      const prettyString = JSON.stringify(json, null, 2);
-      console.log(prettyString);
-    } catch (error) {
-      CustomLog.Error("Invalid JSON provided");
-    }
-  },
+  PrettyJSON: (json) => logJsonWithColor(json),
 };
 
 export default CustomLog;
