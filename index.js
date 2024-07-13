f05f068258eef471d617156ef2338496bebdb2b6
direx99 <dinith.dash@gmail.com>
2024-07-13 20:29:20 +0530

    feat: Success coloed log added.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const logWithColor = (message, color) => {
 
 const CustomLog = {
   Default: (message) => logWithColor(message, ""),
+  Success: (message) => logWithColor(message, "green"),
   Info: (message) => logWithColor(message, "blue"),
   Error: (message) => logWithColor(message, "red"),
   Warn: (message) => logWithColor(message, "orange"),
